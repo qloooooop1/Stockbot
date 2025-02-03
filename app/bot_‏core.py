c5a55310465034c1882e80f60e0bd80b67c86443
qloooooop1 <qloooooop@gmail.com>
2025-02-04 00:47:01 +0300

    Update ⁨bot_‏core.py⁩

--- app/bot_‏core.py
+++ app/bot_‏core.py
@@ -10,10 +10,6 @@ from sqlalchemy import or_
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Bot is running!"
-
 # Custom modules
 from .database import db, ContentRegistry, GlobalImpact
 from .utils.content_filter import classify_content
@@ -426,6 +422,10 @@ class StockBot:
     def run(self):
         self.updater.start_polling()
         self.updater.idle()
+        
+        @app.route('/')
+def home():
+    return "Bot is running!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
