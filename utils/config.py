7380a159feffaf641f513fe8faa5fb385241b8e0
qloooooop1 <qloooooop@gmail.com>
2025-02-04 05:39:05 +0300

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,6 @@
+class Config:
+    TELEGRAM_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+    CONTENT_CATEGORIES = {
+        'stock': ['stock', 'price', 'market'],
+        'global_event': ['event', 'news', 'impact']
+    }
