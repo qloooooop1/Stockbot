1004545adc0f79afaa31f5569f715bbed856d78d
qloooooop1 <qloooooop@gmail.com>
2025-02-05 23:26:31 +0300

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -6,6 +6,14 @@ from sqlalchemy import create_engine
 load_dotenv()
 
 class Config:
+    # إعدادات قاعدة البيانات
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///default.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # إعدادات أخرى حسب الحاجة
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-123')
+    
+class Config:
     # إعدادات تليجرام
     TELEGRAM_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
     ADMIN_IDS = os.getenv('ADMIN_IDS', '').split(',')
