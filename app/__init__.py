0b76271379fc80d4ef1c2e70835a2d51537322d1
qloooooop1 <qloooooop@gmail.com>
2025-02-05 03:48:47 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,34 +8,18 @@ class ContentRegistry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # ... (بقية الحقول)
 
-# إضافة هذه الدالة في الأسفل
 def init_db(app):
     with app.app_context():
         db.create_all()
 
-# أضف في الأعلى:
-from app import app, db  # استيراد من الحزمة الحالية
-from app.utils.config import Config
-from app.utils.content_filter import classify_content
-from app.utils.duplicate_checker import is_duplicate
+def create_app():
+    from flask import Flask
+    app = Flask(__name__)
+    app.config.from_object('app.utils.config.Config')
+    db.init_app(app)
+    
+    # Import modules after app initialization
+    with app.app_context():
+        from app import bot_core, market_data, notifications
 
-# تعديل الاستيرادات الأخرى لتبدأ بـ app.
-from app.market_data import fetch_stock_data
-from app.technical_analysis import generate_ta_report
-from app.Utilities import get_welcome_message
-
-# بقية الكود كما هو...
-from flask import Flask
-from app.database import db
-
-# إنشاء تطبيق Flask
-app = Flask(__name__)
-
-# تحميل الإعدادات
-app.config.from_object('app.utils.config.Config')
-
-# تهيئة قاعدة البيانات
-db.init_app(app)
-
-# استيراد الوحدات بعد التهيئة
-from app import bot_core, market_data, notifications
+    return app
